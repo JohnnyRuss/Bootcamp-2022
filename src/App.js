774a6fb6252ae939c106be77bb5b181544147cf1
Johnny Rusieshvili <georgiaspontoeli123@gmail.com>
2022-09-01 01:17:48 +0400

    split up code with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import {
-  Landing,
-  AddCollaboratorInfo,
-  AddPcInfo,
-  AddNote,
-  ProductsList,
-  ProductPage,
-} from './Pages';
+
+import { Landing, AddNote } from './Pages';
+import { Spinner } from './components/Layouts';
+
+const AddCollaboratorInfo = lazy(() => import('./Pages/AddCollaboratorInfo'));
+const AddPcInfo = lazy(() => import('./Pages/AddPcInfo'));
+const ProductsList = lazy(() => import('./Pages/ProductsList'));
+const ProductPage = lazy(() => import('./Pages/ProductPage'));
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<Spinner />}>
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route path='/add-note' element={<AddNote />}>
@@ -20,7 +21,7 @@ function App() {
         <Route path='/note-list' element={<ProductsList />} />
         <Route path='/note-list/:id' element={<ProductPage />} />
       </Routes>
-    </>
+    </Suspense>
   );
 }
 
